fa7e93113121ebfaaef54a6818c081c7548de68c
LdmlD <luyda.derkach@gmail.com>
2016-08-08 09:00:16 +0000

    First commit

---
+++ example.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+# access_key = "${var.access_key}"
+# secret_key = "${var.secret_key}"
+ region     = "us-west-2"
+}
+
+resource "aws_security_group" "mine" {
+ name = "nginx"
+ description = "Used in the terraform"
+
+ ingress {
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
+
+ ingress {
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
+
+ ingress {
+  from_port = 3306
+  to_port     = 3306
+  protocol    = "tcp"
+  cidr_blocks = ["${var.public_ip}"]
+ }
+ 
+ ingress {
+  from_port = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["${var.public_ip}"]
+ }
+ 
+ egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
+
+}
+
+resource "aws_instance" "mine" {
+ ami           = "${var.ami}"
+ instance_type = "t2.micro"
+ key_name="${var.private_key}"
+ tags {
+  Nginx = "Yes"
+  MySql = "Yes"
+ }
+ connection {
+   user = "ubuntu"
+ }
+ security_groups = ["${aws_security_group.mine.name}"]
+}
+
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+ default = "us-west-2"
+}
+variable "public_ip" {
+ default = "217.20.166.113/32"
+}
+variable "ami" {
+ default = "ami-d732f0b7"
+}
+variable "private_key" {
+ default = "NewKey"
+}
