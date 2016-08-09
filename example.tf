7cb7a07ab1de0b2435e086292fc7b7b3123c5f45
LdmlD <luyda.derkach@gmail.com>
2016-08-09 13:56:42 +0300

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,6 +1,4 @@
 provider "aws" {
- access_key = "${var.access_key}"
- secret_key = "${var.secret_key}"
  region     = "us-west-2"
 }
 
@@ -62,8 +60,6 @@ resource "aws_instance" "mine" {
 resource "terraform_remote_state" "mine" {
     backend = "s3"
     config {
-        access_key = "${var.access_key}"
-        secret_key = "${var.secret_key}"
         bucket = "myterraformremotestate"
         key = "terraform.tfstate"
         region = "us-west-2"
